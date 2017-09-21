d4969e5c6880cd880d1c13e9d052c79a744274d3
JiangZhiYong <359135103@qq.com>
2017-09-21 10:42:45 +0800

    websocket

---
+++ src/main/java/com/jzy/game/engine/mina/websocket/WebSocketCodecFactory.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jzy.game.engine.mina.websocket;
+
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolCodecFactory;
+import org.apache.mina.filter.codec.ProtocolDecoder;
+import org.apache.mina.filter.codec.ProtocolEncoder;
+
+/**
+ * Codec Factory used for creating websocket filter.
+ * 
+ * @author DHRUV CHOPRA
+ */
+public class WebSocketCodecFactory implements ProtocolCodecFactory{
+    private ProtocolEncoder encoder;
+    private ProtocolDecoder decoder;
+
+    public WebSocketCodecFactory() {
+            encoder = new WebSocketEncoder();
+            decoder = new WebSocketDecoder();
+    }
+
+    @Override
+    public ProtocolEncoder getEncoder(IoSession ioSession) throws Exception {
+        return encoder;
+    }
+
+    @Override
+    public ProtocolDecoder getDecoder(IoSession ioSession) throws Exception {
+        return decoder;
+    }    
+}
